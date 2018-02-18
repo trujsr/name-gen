ae66cefcb04bd7495c4191c53a89828c9f0cbb28
trujsr <trujsr@gamil.com>
2018-02-18 08:07:02 +0300

    Test changes

--- name_gen.c
+++ name_gen.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 int main(void)
 {
 	srand(time(NULL));
 	int i;
 	int fl = 1;
-	int vowels[6] = {97, 101, 105, 111, 117, 121};
+	int vowels[6] = {'a', 'e', 'i', 'o', 'u', 'y'};
 	int consolant[20] = 
-	{98, 99, 100, 102, 103, 104, 106, 107, 108, 109, 110, 112, 113,
-	 114, 115, 116, 118, 119, 120, 122};
+	{'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q',
+	 'r', 's', 't', 'v', 'w', 'x', 'z'};
 	
 	const unsigned short min_name_len = 5;
-	const unsigned short max_name_len = 20;
+	const unsigned short max_name_len = 10;
+	
 	unsigned short name_len = min_name_len + rand() % 6;
-	printf("%hu\n", name_len);
+	printf("Length: %hu\n", name_len);
 	char name[max_name_len];
-	name[0] = 'A' + rand() % 26;
-	for (i = 0; i < 6; i++)
+	
+	name[0] = 'A' + rand() % 26;	//first letter
+	
+	for (i = 0; i < 6; i++)			//first letter is vowel?
 	{
 		if (vowels[i] == name[0])
 			fl = 0;
 	}
+	
 	for (i = 1; i < name_len; i++)
 	{
 		if (i % 2 == fl)
@@ -30,6 +35,6 @@ int main(void)
 			name[i] = consolant[rand() % 20];
 	}
 	name[i] = '\0';
-	printf("%s\n", name);
+	printf("Name: %s\n", name);
 	return 0;
 }
